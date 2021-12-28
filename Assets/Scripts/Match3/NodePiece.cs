e3d69c0cec0b125a4e20f3cdd410d60fec423b5c
Everyatis <quentin.garnier@etu.unice.fr>
2021-12-28 17:24:44 +0100

    #4 : mouvements (1/2)
    ça bouge seulement quand on peut et ça détruit les lignes/colonnes de 3 et+ et les carrés

--- Assets/Scripts/Match3/NodePiece.cs
+++ Assets/Scripts/Match3/NodePiece.cs
@@ -2,8 +2,9 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.EventSystems;
 
-public class NodePiece : MonoBehaviour
+public class NodePiece : MonoBehaviour, IPointerDownHandler, IPointerUpHandler
 {
 	public int value;
 	public Point index;
@@ -14,6 +15,7 @@ public class NodePiece : MonoBehaviour
 	[HideInInspector]
 	public RectTransform rect;
 
+	bool updating;
 	Image img;
 
 	public void Initialize(int v, Point p, Sprite piece) {
@@ -35,7 +37,39 @@ public class NodePiece : MonoBehaviour
     	pos = new Vector2(100 * index.x, -100 * index.y);
     }
 
+    public void MovePosition(Vector2 move) {
+    	rect.anchoredPosition += move * Time.deltaTime * 25f;
+    }
+
+    public void MovePositionTo(Vector2 move) {
+    	rect.anchoredPosition = Vector2.Lerp(rect.anchoredPosition, move, Time.deltaTime * 25f);
+    }
+
+    public bool UpdatePiece() {
+    	if(Vector3.Distance(rect.anchoredPosition, pos) > 1) {
+    		MovePositionTo(pos);
+    		updating = true;
+    		return true;
+    	}
+    	else {
+    		rect.anchoredPosition = pos;
+    		updating = false;
+    		return false;
+    	}
+    }
+
     void UpdateName() {
     	transform.name = "Node [" + index.x + ", " + index.y + "]";
+    }
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+    	if(updating) return;
+    	MovePieces.instance.MovePiece(this);
+    }
+
+    public void OnPointerUp(PointerEventData eventData)
+    {
+    	MovePieces.instance.DropPiece();
     }
 }
